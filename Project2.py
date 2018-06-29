0f23cff8ed7cf87834a9e5b778bbc10e9b3d1437
Kevin de Roo <kevinderoo2002@gmail.com>
2018-06-29 17:10:50 +0200

    Add files via upload

---
+++ Project2.py
@@ -0,0 +1,206 @@
+# Importing libraries needed
+import sys
+import csv
+import argparse
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+from datetime import datetime
+
+
+def arguments():
+    # Creating parameters to insert and plot a specified graph
+    parser = argparse.ArgumentParser(usage='-f [] -d [] -m [] -s [] -e [] -a [] -o []')
+    
+    parser.add_argument("-f", "--filename", help="Filename. Path to and with your file name without dataformat", required=True)
+    parser.add_argument("-d", "--dataformat", choices=['nc', 'tss'], help="Dataformat. Choose 'nc' for NetCDF or 'tss' for Timestep files", required=True)
+    parser.add_argument("-m", "--mask", type=int, nargs="+", help="Mask. Either 1 (single point) or 2 points (topleft, bottomright)", required=False)
+    parser.add_argument("-s", "--start_date", help="Start date. Usage: day/month/year", required=True)
+    parser.add_argument("-e", "--end_date", help="End date. Usage: day/month/year", required=True)
+    parser.add_argument("-a", "--aggregation", choices=['yes', 'no'], help="Yearly Aggregation. Choose 'yes' or 'no' to see every year's graph in a single one", required=True)
+    parser.add_argument("-o", "--outfile", help="Outfile. Choose a name for the output file", required=True)
+    parser.add_argument("-i", "--stationid", help="Station ID. Choose a station by giving its number", required=False)
+    
+    
+    args = parser.parse_args()
+    if args.dataformat=='nc' and args.stationid:
+        sys.exit("Dataformat ['nc'] and Station ID can't go together!")
+    elif args.dataformat=='tss' and not args.stationid:
+        sys.exit("Dataformat ['tss'] requires a Station ID!")
+        
+    return args
+
+    
+def print_arguments(args):
+    # Printing the values entered by the user
+    print "X--------------------------------------------X"
+    print ("(Filename)             You entered:  " + str(args.filename))
+    print ("(Dataformat)           You entered:  " + str(args.dataformat))
+    print ("(Mask)                 You entered:  " + str(args.mask))
+    print ("(Start Date)           You entered:  " + str(args.start_date))
+    print ("(End Date)             You entered:  " + str(args.end_date))
+    print ("(Yearly Aggregation)   You entered:  " + str(args.aggregation))
+    print ("(Outfile)              You entered:  " + str(args.outfile))
+    print "X--------------------------------------------X"
+
+
+def define_mask(mask, x, y):
+    # Calculating all variables needed for the mask
+    res = {'x1': None, 'y1': None, 'x2': None, 'y2': None}
+    delta_x = x[1] - x[0]
+    delta_y = y[0] - y[1]
+    if mask:
+        if len(mask) == 4:
+            print "You selected a bounding box mask"
+            res['x1'] = mask[0]
+            res['y1'] = mask[1]
+            res['x2'] = mask[2]
+            res['y2'] = mask[3]
+            res['min_x1'] = int(round((res['x1'] - x[0]) / delta_x))
+            res['min_y1'] = int(round((y[0] - res['y1']) / delta_y))
+            res['min_x2'] = int(round((res['x2'] - x[0]) / delta_x))
+            res['min_y2'] = int(round((y[0] - res['y2']) / delta_y))
+        elif len(mask) == 2:
+            print "You selected a single point filter"
+            res['x1'] = mask[0]
+            res['y1'] = mask[1]
+            res['min_x'] = int(round((res['x1'] - x[0]) / delta_x))
+            res['min_y'] = int(round((y[0] - res['y1']) / delta_y))
+        else:
+            sys.exit("ERROR: Mask usage: 'x1' 'y1' 'x2' 'y2'")
+    print res
+    return res
+
+
+def process_nc(args):
+    # Creating a dataset for the file
+    file_path = (args.filename + "." + args.dataformat)
+    dataframe = Dataset(file_path)
+    
+    if args.aggregation=='yes' and not args.mask:
+        sys.exit("ERROR: Aggregation ['yes'] needs Mask")
+        
+    # Setting up the dataframe
+    x = dataframe["x"]
+    y = dataframe["y"]
+    dis = dataframe["dis"]
+    mask = define_mask(args.mask, x, y)
+    delta_x = x[1] - x[0]
+    delta_y = y[0] - y[1]
+
+    # Defining the start and end date
+    date_format = "%d/%m/%Y"
+    date0 = datetime.strptime('1/1/1990', date_format)
+    date1 = datetime.strptime(args.start_date, date_format)
+    date2 = datetime.strptime(args.end_date, date_format)
+
+    # Checking if 'aggregation' is 'yes'
+    if args.aggregation=='yes' and args.mask:
+        
+        # Getting the timeline from the start date to the end date
+        first_year = date1.year
+        last_year = date2.year
+        date_begin_difference = date1 - date0
+        date_end_difference = date2 - date0
+        date_begin = date_begin_difference.days
+        date_end = date_end_difference.days
+        
+        counter = 1
+        ra = 0
+        ylist = []
+        df_list = []
+        df_year = []
+        
+        # Appending the years/days to a list, keeping leap years in mind
+        for year in xrange(first_year, last_year + 1):
+            counter += 366 if year % 4 == 0 else 365
+            ra = 366 if year % 4 == 0 else 365
+            ylist += [year for b in xrange(ra)]
+                
+            # Plotting the graphs according to the mask
+            if len(args.mask) == 2:
+                df_y = dis[counter - ra:counter, mask['min_y'], mask['min_x']]
+                plt.plot(df_y)
+            elif len(args.mask) == 4:  
+                dfm = dis[counter - ra:counter, int(round(mask['y2'] / delta_y)):int(round(mask['y1'] / delta_y)), int(round(mask['x1'] / delta_x)):int(round(mask['x2'] / delta_x))].mean(axis=0)
+                df_list.append(dfm)
+                df_year.append(year)
+       
+        # If mask length = 2, plot violin graph according to data. If mask length = 4, create an outfile with a piece of a map according to the two points
+        if len(args.mask) == 2:
+            plt.show()
+            df_y = dis[date_begin:date_end, mask['min_y'], mask['min_x']]
+            df_x = pd.DataFrame(ylist, columns=["year"])
+            df_x = df_x["year"][date_begin:date_end]
+            sns.violinplot(x=df_x, y=df_y)
+            plt.show()
+        
+        elif len(args.mask) == 4:
+            dsout = Dataset(args.outfile+".nc", "w", format="NETCDF4_CLASSIC")
+            lat = dsout.createDimension('y', ((mask['y1'] - mask['y2']) / delta_y))
+            lon = dsout.createDimension('x', ((mask['x2'] - mask['x1']) / delta_x))
+            time = dsout.createDimension('time', None)
+        
+            times = dsout.createVariable('time', np.float64, ('time',))
+            latitudes = dsout.createVariable('y', np.float32,('y',))
+            longitudes = dsout.createVariable('x', np.float32,('x',))
+            
+            diss = dsout.createVariable('dis', np.float32, ('time', 'y', 'x',))
+            latitudes[:]=list(range(int(mask['y1']), int(mask['y2']), int(-delta_y)))
+            longitudes[:]=list(range(int(mask['x1']), int(mask['x2']), int(delta_x)))
+            
+            for year in range(len(df_year)):
+                diss[year,:,:] = df_list[year]
+                
+            # Closing the output file
+            dsout.close()
+        
+
+def process_tss(args):
+    
+    # Counting lines and columns in the file
+    file_path = (args.filename + "." + args.dataformat)
+    with open(file_path) as f:
+        count = sum(1 for line in f)
+        f.seek(0)
+        reader = csv.reader(f, delimiter='\t', skipinitialspace=True)
+        first_row = next(reader)
+
+        len_columns = len(first_row)
+    
+        
+    # Reading the file and appending the columns to a dataframe
+    df = pd.read_csv(file_path, header=None, delim_whitespace=True, skiprows=820)
+    col = list()
+    
+    for i in range(len_columns):
+        col.append(i)
+    df.columns=col
+    df = df[1:]
+    df["timestamp"]=pd.date_range(start='1/1/1990', periods=count)
+    lyear = list()
+    for date in pd.date_range(start='1/1/1990', periods=count):
+        lyear.append( )
+    df["year"]=lyear
+    
+    
+def main():
+    
+    # Calling all functions to operate and do the magic
+    args = arguments()
+    dataformat = args.dataformat    
+    print_arguments(args)    
+    
+    # Plotting a graph for the file in two ways; one for a NetCDF file, the other for a '.tss' file.
+    if dataformat == 'nc':
+        process_nc(args)
+    else:
+        process_tss(args)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+    
---
+++ README.txt
@@ -0,0 +1,59 @@
+# I. General Information
+
+**Created:** 29/6/2018
+**Author:** Kevin de Roo (Aged 16)
+**Subject:** Python 2.7 (Graphs)
+**Software used:** Spyder, Jupyter Notebook, Anaconda Navigator, Anaconda Prompt, ArcMap
+**Libraries used:** Pandas, Numpy, Seaborn, Matplotlib, netCDF4
+
+# II. Execution Of The File
+
+## 1. **Parameters**
+Before executing the file, you need to know what the parameters are:
+
+- **Filename** (The path to the file (in this case the discharge file) and the name of the file without dataformat) 
+- **Dataformat** (The dataformat of the file. The user can choose between '**nc**' and '**tss**')
+- **Mask** (The mask for the graph. Either the coordinates for **a single point**, which will plot a line graph, or the coordinates for **two points**, which will give you a piece of a map)
+- **Start Date** (The first date on the graph. Usage is day/month/year.)
+- **End Date** (The last date on the graph. Usage is day/month/year.)
+- **Yearly Aggregation** (View all graphs per year in one single graph. Choose '**yes**' or '**no**')
+- **Outfile** (The name of the outfile. The user can choose any name)
+
+## 2. **Execution of the file**
+To **execute** the file, you can use **Anaconda Prompt**, for example. Just write the following in the prompt:
+
+- `cd [Folder Name]` 'Folder Name' is the folder where the python file is located
+- `python [Python File] -f [Filename] -d [Dataformat] -m [Mask] -s [Start Date] -e [End Date] -a [Yearly Aggregation] -o [Outfile]` 'Python File' is the name of the python file you want to execute, the others are the parameters.
+- **Example1 (1 point):**
+`python PythonFile1.py -f dis -d nc -m 1111111 2222222 -s 1/1/1990 -e 1/1/2000 -a yes -o OutFile1` Mask: x1=1111111, y1=2222222. This will create graphs for the values at those coordinates.
+- **Example2 (2 points):**
+`python PythonFile1.py -f dis -d nc -m 1111111 3333333 2222222 1111111 -s 1/1/1990 -e 1/1/2000 -a yes -o OutFile1` Mask: x1=1111111, y1=3333333, x2=2222222, y2=1111111. This will create a piece of the world map with TopLeft corner (x1,y1) and BottomRight corner (x2,y2).
+
+# III. Quick Introduction
+
+Python is a powerful programming language, being able to do anything the user wishes. This is why I chose to spend my working experience on python, hoping to learn a lot more than I already knew. **Valerio Lorini** and **Peter Salamon**, from unit "**Copernicus E.1**" at the **JRC (Joint Research Centre)** in **Ispra (Italy)** made this possible for me and I am really grateful to them for their support. With that being said, here is a summary of the projects that I was assigned:
+
+## **Project 1:**
+The first thing I did was experiment with new Python **libraries** and commands using **Jupyter Notebook** and **Spyder**, two python editors. Following some tutorials, I couldn't wait to learn more. My first project was to extract information from a text file that I was given. This file contained **discharge values** for many different stations across Europe. Extracting the information and putting them in a **dataframe** was my first problem, since I actually didn't know much about python. Luckily we have modern technology, namely the Internet. During my coding I had to look up many things and it started to give me a clear picture of what python can do. After figuring out how to create the dataframe, I had to make a **timestamp** (timeline), so I could plot a graph. Again, I had to look up a few things, since new commands created new problems or bugs. Finally, after two or three days of coding, I completed my first project. A thing that I could have added are parameters to let the user choose a station and a start/end date. I wasn't confident of my coding skills yet, so I decided not to do it.
+
+## **Project 2:**
+My second project was way more complex. It was basically the same thing, but with a **NetCDF** file and parameters included (which is the project you downloaded). Reading the file and making a dataframe was just one line of code, which really saved a lot of time. Many of the problems I encountered were based on the **complexity of the code**, something that I simply didn't understand, because I hadn't spent much time on understanding python. Nevertheless, with the help of the Internet and my tutor **Valerio Lorini**, I managed to get the code and parameters to work. About **seven days** were spent on the making of this project. I did not have the time to finish the .tss part (I also included a parameter for when you have a .tss file), but the code for a NetCDF file is (pretty much) done.
+
+# IV. To-Do List
+I haven't finished everything yet, so here is a quick to-do list:
+
+- **Remove hardcoded values:** Unfortunately I have no idea how to remove the last hardcoded values, for example the 'dis' variable. This variable will not work for any other file that does not contain discharge values. Another example is the "timestamp" variable. Some files contain values per month, while mine contains values per day.
+- **Finish .tss part:** So far I only had time to extract all information and put it in a dataframe. I still need to plot the graphs according to the parameters and to write some code for the outfile.
+- **Decrease memory usage:** Some lines of code can be done much quicker, reducing the memory used during the execution of the file.
+- **Aggregation = 'no':** I haven't added this part for any of the file types. Basically, when the user chooses 'no' for the Yearly Aggregation, the graphs just have to pop up one after another. If you choose 'no' right now, the execution will finish without doing anything.
+- **Projection:** The projection of the map piece currently doesn't work for **ArcMap**, but it should work for **QGIS**.
+
+# V. Copyright
+
+This script is released under terms of **EUPL License**. The python file is for general use (anyone can feel free to use it), but do not claim it as your own. I have spent many hours on the file, trying to make it as bug-free as possible. You may change the code or anything else, but that does not make you the owner. Thank you in advance.
+
+
+
+
+
+
